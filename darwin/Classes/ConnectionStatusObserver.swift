3ac01401fe583aa34c1e34a776f1e38c3e6bb10a
Tadas Krivickas <tadas.krivickas@gmail.com>
2024-01-19 11:57:14 +0200

    Simplify notification observer using init/deinit

--- darwin/Classes/ConnectionStatusObserver.swift
+++ darwin/Classes/ConnectionStatusObserver.swift
@@ -2,53 +2,50 @@
 import Flutter
 import UIKit
 #elseif os(macOS)
-import FlutterMacOS
 import Cocoa
+import FlutterMacOS
 #else
 #error("Unsupported platform")
 #endif
 
 import NetworkExtension
-import WireGuardKit
 import os
+import WireGuardKit
 
 class ConnectionStatusObserver: NSObject, FlutterStreamHandler {
-
     private var eventSink: FlutterEventSink?
     private var isRunning: Bool = false
+
+    override init() {
+        super.init()
+        NotificationCenter.default.addObserver(
+            forName: NSNotification.Name.NEVPNStatusDidChange,
+            object: nil,
+            queue: OperationQueue.main,
+            using: handleStatusChanged
+        )
+    }
+
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
 
     public func handleStatusChanged(notification: Notification?) {
         guard let conn = notification?.object as? NEVPNConnection else {
             return
         }
-        guard let eventSink = eventSink else {
-            return
-        }
         let newStatus = ConnectionStatus.fromNEVPNStatus(status: conn.status)
 
         Logger.main.debug("VPN status changed: \(newStatus.string())")
-        eventSink(newStatus.string())
+        eventSink?(newStatus.string())
     }
 
     public func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
-        self.eventSink = events
-
-        if !isRunning {
-            NotificationCenter.default.addObserver(
-                forName: NSNotification.Name.NEVPNStatusDidChange,
-                object: nil,
-                queue: OperationQueue.main,
-                using: handleStatusChanged
-            )
-        }
-
-        isRunning = true
+        eventSink = events
         return nil
     }
 
     public func onCancel(withArguments arguments: Any?) -> FlutterError? {
-        isRunning = false
-        NotificationCenter.default.removeObserver(self)
         eventSink = nil
         return nil
     }
