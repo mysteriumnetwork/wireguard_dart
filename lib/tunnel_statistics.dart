ee7db9bd0244cf110d01c57336806912bbbbb6df
Kristijan <kmitrikjeski@gmail.com>
2025-04-30 16:37:38 +0200

    Add const

--- lib/tunnel_statistics.dart
+++ lib/tunnel_statistics.dart
@@ -8,7 +8,7 @@ class TunnelStatistics {
   /// [totalDownload] and [totalUpload] are the total bytes downloaded
   /// and uploaded, respectively. [latestHandshake] is the timestamp of
   /// the latest handshake.
-  TunnelStatistics({
+  const TunnelStatistics({
     required this.totalDownload,
     required this.totalUpload,
     required this.latestHandshake,
@@ -16,7 +16,9 @@ class TunnelStatistics {
 
   /// Factory constructor that creates a [TunnelStatistics] object from a JSON map.
   factory TunnelStatistics.fromJson(Map<String, dynamic> json) => TunnelStatistics(
-      totalDownload: json['totalDownload'] as int, totalUpload: json['totalUpload'] as int, latestHandshake: json['latestHandshake'] as int);
+      totalDownload: json['totalDownload'] as int,
+      totalUpload: json['totalUpload'] as int,
+      latestHandshake: json['latestHandshake'] as int);
 
   /// Converts the [TunnelStatistics] object to a JSON map.
   Map<String, dynamic> toJson() => {
